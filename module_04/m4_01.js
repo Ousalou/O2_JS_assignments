34ada85b6129342e13e5f07cebc3734543c762b3
Ousalou <osalonen@icloud.com>
2025-05-18 23:46:05 +0300

    html_files

--- module_04/m4_01.js
+++ module_04/m4_01.js
@@ -7,11 +7,6 @@
 // </form>
 // Test the form. The result should be a page full of JSON formatted data.
 
-//Develop the app further.
-// Add JavaScript that gets the value entered to the form
-// and sends a request with fetch to https://api.tvmaze.com/search/shows?q=${value_from_input}.
-// Print the search result to the console. (3p)
-
 let form = document.createElement("form")
 form.action = "https://api.tvmaze.com/search/shows"
 
