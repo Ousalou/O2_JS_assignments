63e83b81c2d8253100a6a6108edbbdcb9ab8ea40
Ousalou <osalonen@icloud.com>
2025-05-18 21:52:08 +0300

    m2_01

--- module_03/t4/4.js
+++ module_03/t4/4.js
@@ -1,3 +1,9 @@
+// Open t4 folder in your IDE/editor. Add HTML by using createElement() and appenChild mehtods. (2p)
+// Add the following HTML code to the element with id="target". Add the values from 'students' array to the <option> elements in a for-loop.
+// <option value="2345768">John</option>
+// <option value="2134657">Paul</option>
+// <option value="5423679">Jones</option>
+
 'use strict';
 const students = [
   {
@@ -13,3 +19,14 @@ const students = [
     id: '5423679',
   },
 ];
+
+let select = document.getElementById("target")
+
+for (let i = 0; i < students.length; i++) {
+  let optionCreated = document.createElement("option")
+  optionCreated.textContent = students[i].name
+  optionCreated.value = students[i].id
+  select.appendChild(optionCreated)
+}
+
+
