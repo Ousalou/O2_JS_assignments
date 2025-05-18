e99f10f2775645f9edf2096008a04f38ea6e7767
Ousalou <osalonen@icloud.com>
2025-05-18 21:05:33 +0300

    m2_01

---
+++ module_03/t1/1.js
@@ -0,0 +1,12 @@
+// Open t1 folder in your IDE/editor. Add HTML by using innerHTML property (2p)
+// Add the following HTML code to the element with id="target"
+// <li>First item</li>
+// <li>Second item</li>
+// <li>Third item</li>
+// Add class my-list to the element with id="target"
+
+let list = document.createElement("#target")("li")
+
+list.textContent("First item")
+list.textContent("Second item")
+list.textContent("Third item")
